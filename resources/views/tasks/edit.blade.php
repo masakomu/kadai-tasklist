39ff95cfae5e2e8fc29b15ad8c51b38dcf088959
EC2 Default User <ec2-user@ip-172-31-52-123.ec2.internal>
2018-07-30 12:27:45 +0000

    bootstrap design

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,17 +4,24 @@
 
     <h1>id: {{ $task->id }} のメッセージ編集ページ</h1>
 
+    <div class="row">
+        <div class="col-xs-6">
+            {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
 
-    {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
 
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+                {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
 
-        {!! Form::label('content', 'メッセージ:') !!}
-        {!! Form::text('content') !!}
+            {!! Form::close() !!}
+        </div>
+    </div>
 
-        {!! Form::submit('更新') !!}
-
-    {!! Form::close() !!}
 
 @endsection
